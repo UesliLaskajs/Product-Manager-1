dab13e742daa2871dcb7f6174b5672a38fa7aa8c
Uesli Laska <uesli.laska@gmail.com>
2023-09-17 16:43:04 +0200

    Part 2 of HomeWork

--- Product/server/routes/product.routes.js
+++
@@ -1,6 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-module.exports = (app) => {
-    app.get("/api",ProductController.getData)
-    app.post('/api/product', ProductController.createProduct);     
-}
-
